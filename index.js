6f5573e436c6293cb079e1c723fec17d74a115ce
spikree <avimahalingpure10@gmail.com>
2025-01-21 11:27:39 +0530

    add get all applied jobs endpoint

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDb from "./config/connectDb.js";
 import auth from "./routes/auth.js";
 import profile from "./routes/usersettings/profile.js";
 import resetpassword from "./routes/usersettings/resetpassword.js";
-import {createJob} from "./routes/createJob/createJob.js"
-import { submitProposal } from "./routes/applyJob/applyJob.js";
+import { createJob } from "./routes/createJob/createJob.js";
+import submitProposal from "./routes/applyJob/applyJob.js";
 import { getJobProposals } from "./routes/fetchJobProposals/getProposals.js";
 import getJobs from "./routes/fetchJobs/fetchjobs.js";
 import saveJob from "./routes/saveJobListing/saveJobListing.js";
-import acceptProposal from "./routes/acceptProposal/acceptProposal.js"
-import cancelAcceptedProposal from "./routes/cancelAcceptedProposal/cancelAcceptedProposal.js"
-import cors from "cors"
+import acceptProposal from "./routes/acceptProposal/acceptProposal.js";
+import cancelAcceptedProposal from "./routes/cancelAcceptedProposal/cancelAcceptedProposal.js";
+import cors from "cors";
 import finished from "./routes/finishedJobs/finishedJob.js";
 
 dotenv.config();
@@ -19,27 +19,27 @@ dotenv.config();
 connectDb();
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.use("/auth",auth);
-app.use("/profile",profile)
-app.use("/profile",resetpassword)
-app.use("/job", createJob)
-app.use("/proposal", submitProposal)
-app.use("/job",getJobProposals)
-app.use("/job",getJobs)
-app.use("/job",saveJob);
-app.use("/job",acceptProposal)
-app.use("/job",cancelAcceptedProposal)
-app.use("/job",finished);
+app.use("/auth", auth);
+app.use("/profile", profile);
+app.use("/profile", resetpassword);
+app.use("/job", createJob);
+app.use("/proposal", submitProposal);
+app.use("/job", getJobProposals);
+app.use("/job", getJobs);
+app.use("/job", saveJob);
+app.use("/job", acceptProposal);
+app.use("/job", cancelAcceptedProposal);
+app.use("/job", finished);
 
-app.use("/", (req,res) => {
-    res.json("backend is working")
+app.use("/", (req, res) => {
+  res.json("backend is working");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`port running on http://localhost:${PORT}`);
+  console.log(`port running on http://localhost:${PORT}`);
 });
--- routes/applyJob/applyJob.js
+++ routes/applyJob/applyJob.js
@@ -1,41 +1,83 @@
-import express from "express"
-import Job from "../../models/posting.js"
-import Proposal from "../../models/proposal.js"
-import verifyToken from "../../utils/verifyToken.js"
+import express from "express";
+import Job from "../../models/posting.js";
+import Proposal from "../../models/proposal.js";
+import verifyToken from "../../utils/verifyToken.js";
 
-const submitProposal = express.Router()
+const submitProposal = express.Router();
 
-submitProposal.post("/submit/:id",verifyToken, async (req,res) => {
-    const {id} = req.params; // job id
+submitProposal.post("/submit/:id", verifyToken, async (req, res) => {
+  const { id } = req.params; // job id
+  const { user } = req.user;
+  const { bidAmount, coverLetter } = req.body;
+
+  if (!bidAmount) {
+    return res.status(400).json({ message: "Please enter a bid amount" });
+  }
+
+  if (!coverLetter) {
+    return res.status(400).json({ message: "Please enter a cover letter" });
+  }
+
+  try {
+    const isJob = await Job.findById(id);
+
+    if (!isJob) {
+      return res.status(400).json({
+        message: "Job not found",
+      });
+    }
+
+    const isProposal = await Proposal.findOne({
+      job: id,
+      freelancer: user,
+    });
+
+    if (isProposal) {
+      return res.status(400).json({
+        message: "You have already submitted a proposal for this job",
+      });
+    }
+
+    const proposal = new Proposal({
+      job: id,
+      freelancer: user,
+      bidAmount: bidAmount,
+      coverLetter: coverLetter,
+    });
+
+    await proposal.save();
+
+    res.status(200).json({
+      message: "Proposal submitted successfully",
+      proposal,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Internal server error" });
+  }
+});
+
+submitProposal.get("/getAppliedJobs", verifyToken, async (req,res) => {
     const { user } = req.user;
-    const {bidAmount,coverLetter} = req.body;
 
     try {
-        const isJob = await Job.findById(id)
+        const appliedJobs = await Proposal.find({freelancer: user._id})
 
-        if(!isJob) {
+        if(!appliedJobs || appliedJobs.length === 0){
             return res.status(400).json({
-                message: "Job not found",
+                message: "No Jobs found"
             })
         }
 
-        const proposal = new Proposal({
-            job: id,
-            freelancer: user,
-            bidAmount:bidAmount,
-            coverLetter: coverLetter,
-        })
-
-        await proposal.save();
-
-        res.status(200).json({
-            message: "Proposal submitted successfully",
-            proposal
+        return res.status(200).json({
+            message: "Fetched Applied Jobs",
+            appliedJobs,
         })
     } catch (error) {
-        console.log(error)
-        res.status(400).json({ message: 'Internal server error' });
+        return res.status(500).json({
+            message: "Internal server error"
+        })
     }
-});
+})
 
-export {submitProposal}
+export default submitProposal;
