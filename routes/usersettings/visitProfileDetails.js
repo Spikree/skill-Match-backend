0db4e6b43ababd1d17968c47ab686222995365b9
spikree <avimahalingpure10@gmail.com>
2025-02-07 12:29:14 +0530

    fetched the total ratings and sent the average of the ratings and the count as well

--- routes/usersettings/visitProfileDetails.js
+++ routes/usersettings/visitProfileDetails.js
@@ -1,6 +1,7 @@
 import express from "express";
 import verifyToken from "../../utils/verifyToken.js";
 import User from "../../models/user.js";
+import Rating from "../../models/rating.js";
 
 const visitProfile = express.Router();
 
@@ -15,9 +16,30 @@ visitProfile.get("/getUserProfile/:userId", verifyToken, async (req, res) => {
       });
     }
 
+    
+
+    const ratings = await Rating.find({
+      reviewOf: userDetails._id
+    })
+
+    const totalRatings = ratings.length;
+
+    let averageRating = 0;
+
+    if (totalRatings > 0) {
+      const sumOfRatings = ratings.reduce((sum, rating) => sum + rating.rating, 0);
+      averageRating = (sumOfRatings / totalRatings).toFixed(1); // Rounds to 1 decimal place
+    }
+
+    
+
     return res.status(200).json({
       message: "Fteched user details sucessfully",
       userDetails,
+      ratingStats: {
+        averageRating: parseFloat(averageRating),
+        totalRatings
+      }
     });
   } catch (error) {
     return res.status(500).json({
