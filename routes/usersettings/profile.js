9bbd1529f378189103c02480af0ae219a894d784
spikree <avimahalingpure10@gmail.com>
2025-02-15 21:14:37 +0530

    added rating to the normal user profile

--- routes/usersettings/profile.js
+++ routes/usersettings/profile.js
@@ -1,6 +1,7 @@
 import express from "express";
 import verifyToken from "../../utils/verifyToken.js";
 import User from "../../models/user.js";
+import Rating from "../../models/rating.js";
 
 const profile = express.Router();
 
@@ -36,27 +37,44 @@ profile.put("/edit", verifyToken, async (req, res) => {
   }
 });
 
-profile.get("/getUser", verifyToken, async (req,res) => {
-  const {user} = req.user;
+profile.get("/getUser", verifyToken, async (req, res) => {
+  const { user } = req.user;
 
   try {
     const userDetails = await User.findById(user._id);
 
-    if(!userDetails) {
+    if (!userDetails) {
       return res.status(404).json({
-        message: "User not found"
-      })
+        message: "User not found",
+      });
+    }
+
+    const ratings = await Rating.find({
+      reviewOf: userDetails._id,
+    });
+
+    const totalRatings = ratings.length;
+
+    let averageRating = 0;
+
+    if (totalRatings > 0) {
+      const sumOfRatings = ratings.reduce((sum, rating) => sum + rating.rating, 0);
+      averageRating = (sumOfRatings / totalRatings).toFixed(1); // Rounds to 1 decimal place
     }
 
     return res.status(200).json({
       message: "Fteched user details sucessfully",
-      userDetails
-    })
+      userDetails,
+      ratingStats: {
+        averageRating: parseFloat(averageRating),
+        totalRatings
+      }
+    });
   } catch (error) {
     return res.status(500).json({
-      message: "Internal server error"
-    })
+      message: "Internal server error",
+    });
   }
-})
+});
 
 export default profile;
